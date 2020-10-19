3a3988a8e0a139db47d2b93e9c89dd224f083f49
Ihar Bury <Ihar.Bury@gmail.com>
2020-10-20 00:00:52 +0200

    Initial commit

---
+++ OrlanItemExpansion.lua
@@ -0,0 +1,192 @@
+OrlanItemExpansion_KnownItems = {
+	[20752] = 0, -- Formula: Minor Mana Oil
+	[20753] = 0, -- Formula: Lesser Wizard Oil
+	[20758] = 0, -- Formula: Minor Wizard Oil
+	[21877] = 1, -- Netherweave Cloth
+	[21881] = 1, -- Netherweb Spider Silk
+	[21884] = 1, -- Primal Fire
+	[22307] = 0, -- Pattern: Enchanted Mageweave Pouch
+	[22445] = 1, -- Arcane Dust
+	[22446] = 1, -- Greater Planar Essence
+	[22447] = 1, -- Lesser Planar Essence
+	[22448] = 1, -- Small Prismatic Shard
+	[22449] = 1, -- Large Prismatic Shard
+	[22450] = 1, -- Void Crystal
+	[22452] = 1, -- Primal Earth
+	[22456] = 1, -- Primal Shadow
+	[22573] = 1, -- Mote of Earth
+	[22574] = 1, -- Mote of Fire
+	[22575] = 1, -- Mote of Life
+	[22577] = 1, -- Mote of Shadow
+	[22578] = 1, -- Mote of Water
+	[23445] = 1, -- Fel Iron Bar
+	[23446] = 1, -- Adamantite Bar
+	[23449] = 1, -- Khorium Bar
+	[23500] = 1, -- Saltheril's Haven Party Invitation
+	[23572] = 1, -- Primal Nether
+	[23573] = 1, -- Hardened Adamantite Bar
+	[24477] = 1, -- Jaggal Clam Meat
+	[24478] = 1, -- Jaggal Pearl
+	[29736] = 1, -- Arcane Rune
+	[30183] = 1, -- Nether Vortex
+	[32227] = 1, -- Crimson Spinel
+	[32228] = 1, -- Empyrean Sapphire
+	[32229] = 1, -- Lionseye
+	[32230] = 1, -- Shadowsong Amethyst
+	[32231] = 1, -- Pyrestone
+	[32249] = 1, -- Seaspray Emerald
+	[32428] = 1, -- Heart of Darkness
+	[33226] = 1, -- Tricky Treat
+	[33470] = 2, -- Frostweave Cloth
+	[33864] = 1, -- Brown Brewfest Hat
+	[34052] = 2, -- Dream Shard
+	[34053] = 2, -- Small Dream Shard
+	[34054] = 2, -- Infinite Dust
+	[34055] = 2, -- Greater Cosmic Essence
+	[34056] = 2, -- Lesser Cosmic Essence
+	[34057] = 2, -- Abyss Crystal
+	[34664] = 1, -- Sunmote
+	[35624] = 2, -- Eternal Earth
+	[36909] = 2, -- Cobalt Ore
+	[36912] = 2, -- Saronite Ore
+	[36913] = 2, -- Saronite Bar
+	[36916] = 2, -- Cobalt Bar
+	[37339] = 2, -- Formula: Enchant Weapon - Giant Slayer
+	[37340] = 2, -- Formula: Enchant Chest - Exceptional Resilience
+	[37344] = 2, -- Formula: Enchant Weapon - Icebreaker
+	[37347] = 2, -- Formula: Enchant Cloak - Superior Dodge
+	[37349] = 2, -- Formula: Enchant Cloak - Shadow Armor
+	[37584] = 1, -- Soothing Spearmint Candy
+	[37700] = 2, -- Crystallized Air
+	[38682] = 2, -- Enchanting Vellum
+	[38953] = 2, -- Enchant Gloves - Precision
+	[38960] = 2, -- Enchant Gloves - Gatherer
+	[39004] = 2, -- Enchant Cloak - Wisdom
+	[39354] = 2, -- Light Parchment
+	[41163] = 2, -- Titanium Bar
+	[44471] = 2, -- Formula: Enchant Cloak - Mighty Stamina
+	[44472] = 2, -- Formula: Enchant Cloak - Greater Speed
+	[44473] = 2, -- Formula: Enchant Weapon - Scourgebane
+	[44483] = 2, -- Formula: Enchant 2H Weapon - Massacre
+	[44484] = 2, -- Formula: Enchant Bracer - Greater Assault
+	[44485] = 2, -- Formula: Enchant Gloves - Armsman
+	[44486] = 2, -- Formula: Enchant Weapon - Superior Potency
+	[44487] = 2, -- Formula: Enchant Weapon - Mighty Spellpower
+	[44488] = 2, -- Formula: Enchant Cloak - Wisdom
+	[44489] = 2, -- Formula: Enchant Chest - Powerful Stats
+	[44490] = 2, -- Formula: Enchant Boots - Greater Assault
+	[44491] = 2, -- Formula: Enchant Boots - Tuskarr's Vitality
+	[44492] = 2, -- Formula: Enchant Weapon - Berserking
+	[44494] = 2, -- Formula: Enchant Weapon - Lifeward
+	[44495] = 2, -- Formula: Enchant Weapon - Black Magic
+	[44496] = 2, -- Formula: Enchant Weapon - Accuracy
+	[44498] = 2, -- Formula: Enchant Bracer - Superior Spellpower
+	[44944] = 2, -- Formula: Enchant Bracer - Major Stamina
+	[44947] = 2, -- Enchant Bracer - Major Stamina
+	[45059] = 2, -- Formula: Enchant Staff - Greater Spellpower
+	[45583] = 2, -- Undercity Tabard
+	[45585] = 2, -- Silvermoon City Tabard
+	[52325] = 3, -- Volatile Fire
+	[52328] = 3, -- Volatile Air
+	[52555] = 3, -- Hypnotic Dust
+	[52718] = 3, -- Lesser Celestial Essence
+	[52719] = 3, -- Greater Celestial Essence
+	[52721] = 3, -- Heavenly Shard
+	[52722] = 3, -- Maelstrom Crystal
+	[53010] = 3, -- Embersilk Cloth
+	[64670] = 3, -- Vanishing Powder
+	[69237] = 3, -- Living Ember
+	[71617] = 3, -- Crystallized Firestone
+	[71998] = 3, -- Essence of Destruction
+	[72013] = 3, -- Plans: Foundations of Courage
+	[72988] = 4, -- Windwool Cloth
+	[74247] = 4, -- Ethereal Shard
+	[74248] = 4, -- Sha Crystal
+	[74249] = 4, -- Spirit Dust
+	[74250] = 4, -- Mysterious Essence
+	[74252] = 4, -- Small Ethereal Shard
+	[80433] = 4, -- Blood Spirit
+	[83080] = 4, -- Huojin Tabard
+	[87216] = 4, -- Thermal Anvil
+	[108257] = 5, -- Truesteel Ingot
+	[109076] = 5, -- Goblin Glider Kit
+	[109599] = 5, -- Neural Silencer
+	[109693] = 5, -- Draenic Dust
+	[111245] = 5, -- Luminous Shard
+	[113588] = 5, -- Temporal Crystal
+	[115504] = 5, -- Fractured Temporal Crystal
+	[124121] = 6, -- Wildfowl Egg
+	[124124] = 6, -- Blood of Sargeras
+	[124437] = 6, -- Shal'dorei Silk
+	[124440] = 6, -- Arkhana
+	[124441] = 6, -- Leylight Shard
+	[124442] = 6, -- Chaos Crystal
+	[139004] = 6, -- Hallowed Wand - Banshee
+	[141605] = 6, -- Flight Master's Whistle
+	[141910] = 6, -- Enchant Neck - Mark of the Ancient Priestess
+	[150922] = 6, -- Terrific Tankard O' Terror
+	[156930] = 0, -- Rich Illusion Dust (added in 7.3.5 but intended for Classic)
+	[166444] = 7, -- Honorbound Scouting Map
+	[170170] = 7, -- Fermented Deviate Fish
+	[170172] = 7, -- Brightspine Shell
+	[170177] = 7, -- Exposed Fish
+	[170178] = 7, -- Alpha Fin
+	[170186] = 7, -- Abyss Pearl
+	[170193] = 7, -- Sea Totem
+	[170194] = 7, -- Storm Totem
+	[170200] = 7 -- Seastorm Totem
+}
+
+function OrlanItemExpansion_ToolTipHook(tooltip)
+	local link = select(2, tooltip:GetItem())
+	if not link then
+		return;
+	end;
+	link = select(2, GetItemInfo(link));
+	if not link then
+		return;
+	end;
+	local id = select(5, string.find(link, "|?c?f?f?(%x*)|?H?([^:]*):?(%d+):?(%d*):?(%d*):?(%d*):?(%d*):?(%d*):?(%-?%d*):?(%-?%d*):?(%d*):?(%d*)|?h?%[?([^%[%]]*)%]?|?h?|?r?"));
+	if not id then
+		return;
+	end;
+	id = tonumber(id);
+	if not id then
+		return;
+	end;
+	local expacID;
+	if id < 20604 then
+		expacID = 0;
+	elseif OrlanItemExpansion_KnownItems[id] ~= nil then
+		expacID = OrlanItemExpansion_KnownItems[id];
+	else
+		expacID = select(15, GetItemInfo(id));
+		if (expacID == nil) or (expacID == 0) then
+			GameTooltip:AddLine("Unknown expansion");
+			return;
+		end;
+	end;
+	if expacID == 0 then
+		GameTooltip:AddLine("Classic");
+	elseif expacID == 1 then
+		GameTooltip:AddLine("The Burning Crusade");
+	elseif expacID == 2 then
+		GameTooltip:AddLine("Wrath of the Lich King");
+	elseif expacID == 3 then
+		GameTooltip:AddLine("Cataclysm");
+	elseif expacID == 4 then
+		GameTooltip:AddLine("Mists of Pandaria");
+	elseif expacID == 5 then
+		GameTooltip:AddLine("Warlords of Draenor");
+	elseif expacID == 6 then
+		GameTooltip:AddLine("Legion");
+	elseif expacID == 7 then
+		GameTooltip:AddLine("Battle for Azeroth");
+	elseif expacID == 8 then
+		GameTooltip:AddLine("Shadowlands");
+	else
+		GameTooltip:AddLine("New expansion " .. expacID);
+	end;
+end;
+
+GameTooltip:HookScript("OnTooltipSetItem", OrlanItemExpansion_ToolTipHook);
